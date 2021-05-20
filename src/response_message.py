04b66be8e25572872ca5cddc8b50d60d87bfbfa3
José Antonio Rangel Velázquez <jrangel@ndscognitivelabs.com>
2021-05-20 10:22:54 -0500

    initial commit

--- src/response_message.py
+++ src/response_message.py
@@ -1,8 +1,15 @@
 from json import loads
 from random import randint
 from src import utils_sms
+import os
+import pandas as pd
+from datetime import datetime
 
 RESPONSES = loads(open('src/responses.json', 'r', encoding="utf-8").read())
+ID = "5539518126"
+outname = ID + '.csv'
+outdir = './src/logs'
+fullname = os.path.join(outdir, outname)
 
 def categorize_msg(message:str):
     message = message.lower()
@@ -33,6 +40,13 @@ def process_response(message:str):
     category = categorize_msg(message)
     index_resp = randint(0, len(RESPONSES[category])-1)
     resp = ''
+    Path = os.path.isfile('/scr/logs/'+ ID +'.csv')
+    now = datetime.now()    
+    if os.path.exists(fullname):
+        df = pd.read_csv(fullname)
+        last_category = df.status.values
+        print(last_category[-1])
+        
     if(category == "saludos"):
         resp = processing_hello(message)
         if("Thanks" not in resp):
@@ -45,6 +59,18 @@ def process_response(message:str):
     elif(category == "error"):
         resp = RESPONSES[category][index_resp]
 
+    create_csv(ID, category, now)
     return resp
 
-    
+def create_csv( ID, status, resp_time):
+    timestamp = datetime.timestamp(resp_time)
+    if os.path.exists(fullname):
+        df = pd.read_csv(fullname)
+        data = pd.Series([ID, status, timestamp], 
+                         index=['ID', 'status', 'timestamp'])
+        df = df.append(data, ignore_index=True)
+    else:
+        data = [ID, status, timestamp]
+        columns = ['ID', 'status', 'timestamp']
+        df = pd.DataFrame(data, columns).T
+    df.to_csv(fullname , index=False)
