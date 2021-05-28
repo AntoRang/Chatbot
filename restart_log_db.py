5cd716fb9fe76d3e9a3a2ba1fd789eebbd53cd14
BuildTools <ercv106@hotmail.com>
2021-05-28 15:53:44 -0500

    update by Eric

--- restart_log_db.py
+++ restart_log_db.py
@@ -12,7 +12,7 @@ def main():
     print(warn_text)
     usr = str()
     while usr.lower() != 'y':
-        usr = str(input("Esata seguro que deséa ELIMINAR la Log DB (y/n): " ))
+        usr = str(input("Esata seguro que quiere ELIMINAR la Log DB? (y/n): " ))
     DB.delete_logs()
 
 main()
